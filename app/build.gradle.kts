5289249a668f998e860b7416c8e5dafb3bd116b8
Bayu Stevanus Gerard Simamora <61579584+BayuBatam2008@users.noreply.github.com>
2025-09-18 19:40:50 +0700

    Remove stub Material3 style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,19 +78,25 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-runtime:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-service:2.8.6")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
     implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.savedstate:savedstate-ktx:1.2.1")
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    implementation("commons-net:commons-net:3.9.0")
     implementation("org.apache.commons:commons-net:3.9.0")
 
     implementation("androidx.work:work-runtime-ktx:2.9.1")
